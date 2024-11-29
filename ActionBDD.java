5221a2fa5c7143a0bd50f5d54d99b89cdf766ba7
Priyank Solanki <psolanki@condorcet93.fr>
2024-11-29 16:06:22 +0100

    interface actionBDD added

---
+++ ActionBDD.java
@@ -0,0 +1,7 @@
+public interface ActionBDD {
+    public void afficherProgrammeurs();
+    public void afficherProgrammeur();
+    public void supprimerProgrammeur();
+    public void ajouterProgrammeur();
+    public void modifierSalaire();
+}
---
+++ ActionsBDDImpl.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+
+public class ActionsBDDImpl implements ActionBDD {
+    @Override
+    public void afficherProgrammeurs() {
+        //TODO
+    }
+
+    @Override
+    public void afficherProgrammeur() {
+        //TODO
+    }
+
+    @Override
+    public void supprimerProgrammeur() {
+        //TODO
+    }
+
+    @Override
+    public void ajouterProgrammeur() {
+        //TODO
+    }
+
+    @Override
+    public void modifierSalaire() {
+        //TODO
+    }
+}
